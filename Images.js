93a65ec14c16e35e464b29bd1ab6b38a6a11c77d
damoski2 <akogunoyindamola42@gmail.com>
2022-06-23 19:56:59 +0100

    addicons

--- Images.js
+++ Images.js
@@ -1,3 +1,11 @@
 export const Images = {
-    logo: ''
+    logo: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010434/Asset_1_3x_2_gxxrev.svg',
+    leftSlideIcom: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010433/Group_36_wsx05d.svg',
+    rightSlideIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010433/Group_34_hav2hu.svg',
+    resumeIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010433/resume_1_svdwkd.svg',
+    houseIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010433/building_1_ovdnvs.svg',
+    phoneIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010435/Vector_1_gapacf.svg',
+    playIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010435/Vector_mfpjbo.svg',
+    universityIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010435/university_1_uetnin.svg',
+    communityIcon: 'https://res.cloudinary.com/oyindacodes/image/upload/v1656010433/online-community_1_ekqiyq.svg'
 }
