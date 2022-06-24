e59d356f8809f9c189c3c7462dae3f7f7456b578
damoski2 <akogunoyindamola42@gmail.com>
2022-06-24 12:42:01 +0100

    markers

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -7,11 +7,15 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import { Carousel, Metrics, About, Services } from "../components/import";
 
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 export default function Home() {
   const ref = useRef(null);
   const ref2 = useRef(null);
+
+  useEffect(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+  },[])
 
   //Gsap Animations
   const scrollTrigger = ScrollTrigger.create({
@@ -19,18 +23,12 @@ export default function Home() {
     start: "top top", 
     pin: true, 
     pinSpacing: false ,
-    markers:true
+    markers:true,
   });
 
   gsap.to(ref2.current,{
-    scrollTrigger: {
-      trigger: '.animating-element', //this is the element that will trigger the animation
-      start: 'top center', //this string defines the trigger and scroller start
-      end: 'bottom center', //this string defines the trigger and scroller end
-      scrub: true //this tells GSAP to link animation progress to scroll progress
-    },
+    scrollTrigger: scrollTrigger,
     y: '50%', //translates the element downwards by 50%
-    color: 'red' //transitions the color to red
   })
 
 
